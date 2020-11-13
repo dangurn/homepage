db4ea1a63e871fab557bfd16bfabeedacfcfef68
Daniel Gurney <daniel.gurney@gmail.com>
2020-11-13 12:38:23 +0000

    Some basic customisation

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.scss';
 const App = () => (
 	<div className="App">
 		<header>
-		<p>
-			Welcome to DAN.MEDIA. This site is under development.
-		</p>
+			<p>
+				Welcome to DAN.<span className='orange'>MEDIA</span>, the website for Digital Media Specialist, Daniel Gurney.
+			</p>
+			<p>
+				This site is under development.
+			</p>
 		</header>
 	</div>
 );
